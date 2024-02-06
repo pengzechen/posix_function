e59e44f0094637cb38192c831e4d56180c32049e
zechen <927068267@qq.com>
2024-02-06 10:27:07 +0800

    debug

--- c_file/strtok_r.c
+++ c_file/strtok_r.c
@@ -1,55 +1,14 @@
-#include <stdio.h>
-
-#include <string.h>
-
-
-int main() {
-
-    // ����һ����������ո�ָ����ʵ��ַ���
-
-    char str[] = "Hello World This is a demo";
-
-
-
-    // ʹ��strtok_r()�����ָ��ַ���
-
-    char *token;
-
-    char *save_ptr;
-
-    int word_count = 0;
-
-
-
-    // ��һ�ηָ�
-
-    token = strtok_r(str, " ", &save_ptr);
-
-    while (token != NULL) {
-
-        // ��ӡ�ָ��ĵ���
-
-        printf("���� %d: %s\n", word_count + 1, token);
-
-
-
-        // ���±���ָ���λ��
-
-        save_ptr = token;
-
-
-
-        // �����ָ���һ������
-
-        token = strtok_r(save_ptr, " ", &save_ptr);
-
-        word_count++;
-
-    }
-
-
-
-    return 0;
-
+#include <stdio.h> 
+#include <string.h> 
+  
+int main() 
+{ 
+    char str[] = "Geeks for Geeks"; 
+    char* token; 
+    char* rest = str; 
+  
+    while ((token = strtok_r(rest, " ", &rest))) 
+        printf("%s\n", token); 
+  
+    return (0); 
 }
-
